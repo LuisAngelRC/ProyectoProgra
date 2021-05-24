e32e1e6c9c27fb7c72429b89fd3a34bb214e40fe
MaxGonzalez67304 <al263834@edu.uaa.mx>
2021-05-24 18:31:18 -0500

    escritorio

--- src/escritorio/DesktopPane.java
+++ src/escritorio/DesktopPane.java
@@ -4,6 +4,7 @@ package escritorio;
 public class DesktopPane {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Esta cosa no jala");
     }
     
 
