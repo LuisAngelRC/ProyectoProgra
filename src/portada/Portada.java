6c7c3af1b42ad6425db7809d6398295b8631627d
Ernesto <al226977@edu.uaa.mx>
2021-05-24 18:26:36 -0500

    Portada

--- src/portada/Portada.java
+++ src/portada/Portada.java
@@ -8,7 +8,7 @@ public class Portada {
     public static void main(String[] args) {
         //HOLA 
         System.out.println("Hola mundo");
-        
+        System.out.println("El max no puede subir jajajjaajaj que risa ");
     }
     
 }
